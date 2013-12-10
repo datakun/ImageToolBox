06e52acd4ef592b25ef93468709f8001493d72e0
sigmadream <sigmadream@gmail.com>
2013-12-11 00:42:47 +0900

    Make CH06 Dialog

--- ImageToolBox/Resource.h
+++ ImageToolBox/Resource.h
@@ -2,19 +2,25 @@
 // Microsoft Visual C++ generated include file.
 // Used by ImageToolBox.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_ImageToolBoxTYPE				130
-#define ID_WINDOW_MANAGER			131
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ImageToolBoxTYPE            130
+#define ID_WINDOW_MANAGER               131
+#define IDD_BRIGHTNESS                  310
+#define IDD_CONTRAST                    311
+#define IDD_FILE_NEW                    312
+#define IDD_GAMMA_CORRECTION            313
+#define IDD_DIALOG2                     314
+#define IDD_HISTOGRAM                   314
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        315
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
